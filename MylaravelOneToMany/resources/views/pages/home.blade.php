52c2a9f03c9aa287ca474e1eadabd72edf341958
Mirko <mirkosbaglia2001@gmail.com>
2021-05-31 15:04:41 +0200

    Creazione pagine

---
+++ MylaravelOneToMany/resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main-layout')
+@section('content')
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-12">
+       <h1>hello world</h1>
+      </div>
+    </div>
+  </div>
+@endsection
